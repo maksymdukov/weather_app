db9d123476e3b04e2f62e5e831b34b56a8c3abf2
Maksym Dukov <maxopowerful@gmail.com>
2019-03-03 11:16:39 +0200

    Updates

---
+++ precache-manifest.737981d662aa5f8168b10ccfacf8a28a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0c19f833240749681f5",
+    "url": "/weather_app/static/js/runtime~main.b0c19f83.js"
+  },
+  {
+    "revision": "a0312cb26ab7e1cd9fd9",
+    "url": "/weather_app/static/js/main.a0312cb2.chunk.js"
+  },
+  {
+    "revision": "da126488d688e7de1098",
+    "url": "/weather_app/static/js/2.da126488.chunk.js"
+  },
+  {
+    "revision": "a0312cb26ab7e1cd9fd9",
+    "url": "/weather_app/static/css/main.ffea5354.chunk.css"
+  },
+  {
+    "revision": "f264f3f95d6e626249124214ea5b48af",
+    "url": "/weather_app/index.html"
+  }
+];
